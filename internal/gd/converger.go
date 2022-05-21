c73432efd7613f9e812cfd9122c3f3f3851a3738
Ernest Nguyen <ernest.nguyen.hung@ingrid.com>
2022-05-22 01:31:27 +0200

    Add ctx handling

--- internal/gd/converger.go
+++ internal/gd/converger.go
@@ -41,9 +41,14 @@ func NewConverger(ct options.ConverganceType, ci float64, c CostFunc) (Converger
 // It converges after i iterations.
 func convergeAfter(ctx context.Context, s Stepper, i int) ([]float64, error) {
 	for k := 0; k < i; k++ {
-		err := s.TakeStep()
-		if err != nil {
-			return nil, err
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		default:
+			err := s.TakeStep()
+			if err != nil {
+				return nil, err
+			}
 		}
 	}
 	return s.CurrentCoefficients(), nil
@@ -54,22 +59,27 @@ func convergeAfter(ctx context.Context, s Stepper, i int) ([]float64, error) {
 func convergeAutomatically(ctx context.Context, s Stepper, c CostFunc, t float64) ([]float64, error) {
 	var coeffs []float64
 	for {
-		coeffs = s.CurrentCoefficients()
-		err := s.TakeStep()
-		if err != nil {
-			return nil, err
-		}
-		// Check if cost function decreases by lower value than established threshold.
-		oc, err := c(s.X(), s.Y(), coeffs)
-		if err != nil {
-			return nil, err
-		}
-		nc, err := c(s.X(), s.Y(), s.CurrentCoefficients())
-		if err != nil {
-			return nil, err
-		}
-		if r := 1 - nc/oc; r > 0 && r < t {
-			return s.CurrentCoefficients(), nil
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		default:
+			coeffs = s.CurrentCoefficients()
+			err := s.TakeStep()
+			if err != nil {
+				return nil, err
+			}
+			// Check if cost function decreases by lower value than established threshold.
+			oc, err := c(s.X(), s.Y(), coeffs)
+			if err != nil {
+				return nil, err
+			}
+			nc, err := c(s.X(), s.Y(), s.CurrentCoefficients())
+			if err != nil {
+				return nil, err
+			}
+			if r := 1 - nc/oc; r > 0 && r < t {
+				return s.CurrentCoefficients(), nil
+			}
 		}
 	}
 }
